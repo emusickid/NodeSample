94030e14ad9e82bd3ed13c65b8408145bc14c59c
Eric Loflin <ericloflin@gmail.com>
2015-03-07 23:10:43 -0600

    saving

---
+++ sql/tableau-views.sql
@@ -0,0 +1,18 @@
+﻿SELECT 
+    _sites.name AS SiteName,
+    _projects.name AS ProjectName,
+    _workbooks.name AS WorkbookName,
+    _views.name AS ViewName,
+    _views.*
+FROM 
+    _sites
+    INNER JOIN _projects ON _sites.id = _projects.site_id
+    INNER JOIN _workbooks ON _projects.id = _workbooks.project_id
+    INNER JOIN _views ON _workbooks.id = _views.workbook_id
+WHERE
+    _sites.name = 'POC' AND
+    _projects.name NOT IN ('default', 'Tableau Samples');
+    
+
+
+SELECT * FROM _sites;
