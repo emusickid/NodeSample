ddad6b70c96b3ef7be7e615fed3312020605a68d
Eric Loflin <ericloflin@gmail.com>
2015-03-10 12:46:08 -0500

    saving

--- sql/tableau-views.sql
+++ sql/tableau-views.sql
@@ -1,7 +1,7 @@
-﻿SELECT 
+﻿SELECT DISTINCT
     _sites.name AS SiteName,
     _projects.name AS ProjectName,
-    _workbooks.name AS WorkbookName,
+    --_workbooks.name AS WorkbookName,
     _views.name AS ViewName,
     _views.*
 FROM 
@@ -10,10 +10,12 @@ FROM
     INNER JOIN _workbooks ON _projects.id = _workbooks.project_id
     INNER JOIN _views ON _workbooks.id = _views.workbook_id
 WHERE
-    _sites.name = 'POC' AND
-    _projects.name NOT IN ('default', 'Tableau Samples');
+    _sites.name = 'POC' --AND
+    --_projects.name NOT IN ('default', 'Tableau Samples')
+ORDER BY ViewName
 
-SELECT next_gen_permissions.*, _views.name AS ViewName, _groups.name AS GroupName
+
+SELECT DISTINCT  _views.name AS ViewName
 FROM 
     next_gen_permissions
     INNER JOIN _views ON next_gen_permissions.authorizable_id = _views.id
@@ -23,16 +25,31 @@ FROM
 WHERE authorizable_type = 'View' AND
       grantee_type = 'Group' AND
       _users.name = 'poc_agusa'
-
-SELECT * FROM next_gen_permissions
-
-SELECT * FROM _users;
-
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE column_name LIKE '%permission%';
+ORDER BY 1
 
 
-SELECT datname,procpid,current_query FROM pg_stat_activity;
 
+SELECT DISTINCT
+    _sites.name AS SiteName,
+    _projects.name AS ProjectName,
+    _workbooks.name AS WorkbookName,
+    _views.name AS ViewName
+FROM 
+    _sites
+    INNER JOIN _projects ON _sites.id = _projects.site_id
+    INNER JOIN _workbooks ON _projects.id = _workbooks.project_id
+    INNER JOIN _views ON _workbooks.id = _views.workbook_id
+    INNER JOIN next_gen_permissions perm ON _views.id = perm.authorizable_id
+    INNER JOIN _groups ON perm.grantee_id = _groups.id
+    INNER JOIN group_users ON _groups.id = group_users.group_id
+    INNER JOIN _users ON group_users.user_id = _users.id
+WHERE
+    _sites.name = 'POC'
+    AND _projects.name NOT IN ('default', 'Tableau Samples')
+    AND perm.authorizable_type = 'View'
+    AND perm.grantee_type = 'Group'
+    AND _users.name = 'poc_agusa'
+ORDER BY ViewName
 
 
 
