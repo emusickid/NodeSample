b531bce58150a84d6bce79660602af6d39848d67
e.loflin <emusickid@gmail.com>
2015-04-10 11:51:03 -0500

    saving

--- routes/index.js
+++ routes/index.js
@@ -28,186 +28,215 @@ router.get('/index/:site', function(req, res){
     });
 })
 
-router.get('/projects', function(req, res){
+// router.get('/projects', function(req, res){
 
-	var results = [];
+// 	var results = [];
 
-    // Get a Postgres client from the connection pool
-    var client = new pg.Client(connectionString);
+//     // Get a Postgres client from the connection pool
+//     var client = new pg.Client(connectionString);
 
-    client.connect(function(err) {
+//     client.connect(function(err) {
 		
-	    // SQL Query > Select Data
-	    var sql = 
-		'SELECT ' +
-		   '_projects.name ' +
-		'FROM ' +
-		    '_sites ' +
-		    'INNER JOIN _projects ON _sites.id = _projects.site_id ' +
-		'WHERE ' +
-		    '_sites.name = $1 ' +
-		    'AND _projects.name NOT IN ($2, $3)';
+// 	    // SQL Query > Select Data
+// 	    var sql = 
+// 		'SELECT ' +
+// 		   '_projects.name ' +
+// 		'FROM ' +
+// 		    '_sites ' +
+// 		    'INNER JOIN _projects ON _sites.id = _projects.site_id ' +
+// 		'WHERE ' +
+// 		    '_sites.name = $1 ' +
+// 		    'AND _projects.name NOT IN ($2, $3)';
 
 		
-	    var query = client.query(
-			sql, ['POC', 'default', 'Tableau Samples']);
+// 	    var query = client.query(
+// 			sql, ['POC', 'default', 'Tableau Samples']);
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
+//         // Stream results back one row at a time
+//         query.on('row', function(row) {
+//             results.push(row);
+//         });
 
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            client.end();
-            return res.json(results);
-        });
+//         // After all data is returned, close connection and return results
+//         query.on('end', function() {
+//             client.end();
+//             return res.json(results);
+//         });
 
-        // Handle Errors
-        if(err) {
-          console.log(err);
-        }
+//         // Handle Errors
+//         if(err) {
+//           console.log(err);
+//         }
 
-    });
-});
+//     });
+// });
 
-router.get('/sites', function(req, res){
+// router.get('/sites', function(req, res){
 
-	var results = [];
+// 	var results = [];
 
-    // Get a Postgres client from the connection pool
-    var client = new pg.Client(connectionString);
+//     // Get a Postgres client from the connection pool
+//     var client = new pg.Client(connectionString);
 
-    client.connect(function(err) {
+//     client.connect(function(err) {
 		
-	    // SQL Query > Select Data
-	    var sql = 'SELECT name FROM _sites WHERE status = $1 AND name NOT IN ($2);'
+// 	    // SQL Query > Select Data
+// 	    var sql = 'SELECT name FROM _sites WHERE status = $1 AND name NOT IN ($2);'
 		
 		
-	    var query = client.query(
-			sql, ['active', 'Default']);
+// 	    var query = client.query(
+// 			sql, ['active', 'Default']);
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
+//         // Stream results back one row at a time
+//         query.on('row', function(row) {
+//             results.push(row);
+//         });
 
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            client.end();
-            return res.json(results);
-        });
+//         // After all data is returned, close connection and return results
+//         query.on('end', function() {
+//             client.end();
+//             return res.json(results);
+//         });
 
-        // Handle Errors
-        if(err) {
-          console.log(err);
-        }
+//         // Handle Errors
+//         if(err) {
+//           console.log(err);
+//         }
 
-    });
-});
+//     });
+// });
 
-router.get('/views/:site/:username', function(req, res){
+// router.get('/views/:site', function(req, res){
 
-	// console.log(req.query.username);
-	var site = req.params.site;
-	var username = req.params.username;
+// 	var site = req.params.site;
+	
+// 	var results = [];
+    
+//     // Get a Postgres client from the connection pool
+//     var client = new pg.Client(connectionString);
 
-	// console.log(site);
-	// console.log(username);
-	var results = [];
-    // Get a Postgres client from the connection pool
-    var client = new pg.Client(connectionString);
-
-    client.connect(function(err) {
+//     client.connect(function(err) {
 		
-	    // SQL Query > Select Data
-	    var sql = 
-			'SELECT DISTINCT ' + 
-				'_views.name AS ViewName, ' +
-				'_views.view_url AS ViewURL ' +
-			'FROM ' +
-				'_sites ' +
-				'INNER JOIN _projects ON _sites.id = _projects.site_id ' +
-				'INNER JOIN _workbooks ON _projects.id = _workbooks.project_id ' +
-				'INNER JOIN _views ON _workbooks.id = _views.workbook_id ' +
-				'INNER JOIN next_gen_permissions perm ON _views.id = perm.authorizable_id ' +
-				'INNER JOIN _groups ON perm.grantee_id = _groups.id ' +
-				'INNER JOIN group_users ON _groups.id = group_users.group_id ' +
-				'INNER JOIN _users ON group_users.user_id = _users.id ' +
-			'WHERE ' +
-				'_sites.name = $1 ' +
-				'AND _projects.name NOT IN ($2, $3) ' +
-				'AND perm.authorizable_type = $4 ' +
-				'AND perm.grantee_type = $5 ' +
-				'AND _users.name = $6 ' +
-			'';
+// 	    // SQL Query > Select Data
+// 	    var sql = 
+// 			'SELECT DISTINCT ' + 
+// 				'_views.name AS ViewName, ' +
+// 				'_views.view_url AS ViewURL ' +
+// 			'FROM ' +
+// 				'_sites ' +
+// 				'INNER JOIN _projects ON _sites.id = _projects.site_id ' +
+// 				'INNER JOIN _workbooks ON _projects.id = _workbooks.project_id ' +
+// 				'INNER JOIN _views ON _workbooks.id = _views.workbook_id ' +
+// 				'INNER JOIN next_gen_permissions perm ON _views.id = perm.authorizable_id ' +
+// 				'INNER JOIN _groups ON perm.grantee_id = _groups.id ' +
+// 				'INNER JOIN group_users ON _groups.id = group_users.group_id ' +
+// 				'INNER JOIN _users ON group_users.user_id = _users.id ' +
+// 			'WHERE ' +
+// 				'_sites.name = $1 ' +
+// 				'AND _projects.name NOT IN ($2, $3) ' +
+// 				'AND perm.authorizable_type = $4 ' +
+// 				'AND perm.grantee_type = $5 ' +
+// 				'AND _users.name = $6 ' +
+// 			'';
 
 		   
-	    var query = client.query(sql, [site, 'default', 'Tableau Samples', 'View', 'Group', username]);
+// 	    var query = client.query(sql, [site, 'default', 'Tableau Samples', 'View', 'Group', username]);
 
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            console.log(row);
-            results.push(row);
-        });
+//         // Stream results back one row at a time
+//         query.on('row', function(row) {
+//             console.log(row);
+//             results.push(row);
+//         });
 
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            client.end();
-            return res.json(results);
-        });
+//         // After all data is returned, close connection and return results
+//         query.on('end', function() {
+//             client.end();
+//             return res.json(results);
+//         });
 
-        // Handle Errors
-        if(err) {
-          console.log(err);
+//         // Handle Errors
+//         if(err) {
+//           console.log(err);
+//         }
+
+//     });
+// });
+
+router.get('/viewthumbs/:site/:projectname', function(req, res){
+
+    getProjectView(req.params.site, function(results){
+
+        var viewsToRender = [];
+        for(var i = 0; i < results.length; i++){
+            //console.log(results[i].name);
+
+            if(results[i].name === req.params.projectname){
+                viewsToRender.push(results[i]);
+            };
         }
+        //res.send(viewsToRender[0]);
+        var thumbs = [];
 
+        thumbs.push({thumburl: 'http://devsqltest/t/Allegro/thumb/views/Markets/IndexPriceDataComparison', viewname: 'Index Price Data Comparison'});
+        thumbs.push({thumburl: 'http://devsqltest/t/Allegro/thumb/views/Markets/MarketPriceHeatMap', viewname: 'Market Price Heat Map'});
+        thumbs.push({thumburl: 'http://devsqltest/t/Allegro/thumb/views/Markets/MarketPriceIndexSpread', viewname: 'Market Price Index Spread'});
+        thumbs.push({thumburl: 'http://devsqltest/t/Allegro/thumb/views/Markets/MarketsDashboard', viewname: 'Markets Dashboard'});
+        thumbs.push({thumburl: 'http://devsqltest/t/Allegro/thumb/views/Markets/MarketPricingAnalysis', viewname: 'Market Pricing Analysis'});
+        thumbs.push({thumburl: 'http://devsqltest/t/Allegro/thumb/views/Markets/VolatilityMetrics', viewname: 'Market Pricing Analysis'});
+
+        res.render('reportviews', {
+        views: thumbs,
+        projects: results,
+        appname: 'Allegro Insight'
     });
+    })
+   
 });
 
 
-function getProjects(callback){
-	var results = [];
 
-    // Get a Postgres client from the connection pool
-    var client = new pg.Client(connectionString);
+// function getProjects(callback){
+// 	var results = [];
 
-    client.connect(function(err) {
+//     // Get a Postgres client from the connection pool
+//     var client = new pg.Client(connectionString);
+
+//     client.connect(function(err) {
 		
-	    // SQL Query > Select Data
-	    var sql = 
-		'SELECT ' +
-		   '_projects.name ' +
-		'FROM ' +
-		    '_sites ' +
-		    'INNER JOIN _projects ON _sites.id = _projects.site_id ' +
-		'WHERE ' +
-		    '_sites.name = $1 ' +
-		    'AND _projects.name NOT IN ($2, $3) ' + 
-		'ORDER BY name';
+// 	    // SQL Query > Select Data
+// 	    var sql = 
+// 		'SELECT ' +
+// 		   '_projects.name ' +
+// 		'FROM ' +
+// 		    '_sites ' +
+// 		    'INNER JOIN _projects ON _sites.id = _projects.site_id ' +
+// 		'WHERE ' +
+// 		    '_sites.name = $1 ' +
+// 		    'AND _projects.name NOT IN ($2, $3) ' + 
+// 		'ORDER BY name';
 
 		
-	    var query = client.query(
-			sql, ['testsite', 'default', 'Tableau Samples']);
+// 	    var query = client.query(
+// 			sql, ['testsite', 'default', 'Tableau Samples']);
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
+//         // Stream results back one row at a time
+//         query.on('row', function(row) {
+//             results.push(row);
+//         });
 
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            client.end();
-            callback(results);
-        });
+//         // After all data is returned, close connection and return results
+//         query.on('end', function() {
+//             client.end();
+//             callback(results);
+//         });
 
-        // Handle Errors
-        if(err) {
-          console.log(err);
-        }
-    });
-}
+//         // Handle Errors
+//         if(err) {
+//           console.log(err);
+//         }
+//     });
+// }
 function getSites(callback){
 	var results = [];
 
@@ -278,13 +307,15 @@ function getProjectView(site, callback){
         query.on('row', function(row) {
             
             var match = false;
-            var urlPrefix = site + '/views/'
+            var urlPrefix = site + '/views/';
+            var thumbUrlPrefix = site + '/thumb/views/';
+
             for(var i =0 ; i< projects.length; i++){
 
                 // console.log('made it here -' + i);
 
                 if(projects[i].name === row.projectname){
-                    projects[i].views.push({viewname: row.viewname, viewurl: urlPrefix + row.viewurl});
+                    projects[i].views.push({viewname: row.viewname, viewurl: urlPrefix + row.viewurl, thumburl: thumbUrlPrefix + row.viewurl});
                     match = true;
                 }
             }
@@ -292,7 +323,7 @@ function getProjectView(site, callback){
             if(!match){
                 var newProject = {};
                 newProject.name = row.projectname;
-                newProject.views = [ {viewname: row.viewname, viewurl: urlPrefix + row.viewurl}];
+                newProject.views = [ {viewname: row.viewname, viewurl: urlPrefix + row.viewurl, thumburl: thumbUrlPrefix + row.viewurl}];
                 projects.push(newProject);
             }
 
